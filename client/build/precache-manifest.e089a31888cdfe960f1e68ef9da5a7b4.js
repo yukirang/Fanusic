a83d771ff0e24288f01257fe90a025410fdbf651
yukirang <rangk@163.com>
2019-07-22 23:21:45 -0700

    point

---
+++ client/build/precache-manifest.e089a31888cdfe960f1e68ef9da5a7b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f0314f8bd34e286c02f01c5e185568",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0cc7a7e2abdacb57afa",
+    "url": "/static/css/main.6117ae78.chunk.css"
+  },
+  {
+    "revision": "a5260eff201b59abb11b",
+    "url": "/static/js/2.5ceb2e70.chunk.js"
+  },
+  {
+    "revision": "a0cc7a7e2abdacb57afa",
+    "url": "/static/js/main.8bf970ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const DashboardActions = () => {
   return (
     <div className='dash-buttons'>
-      <Link to='/edit-profile' className='btn btn-light'>
+      <Link to='/edit-profile' className='btn btn-primary'>
         Edit Profile
       </Link>
     </div>
