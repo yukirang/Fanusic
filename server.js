6a0dc89cd42e62cf08c9c8361ec59f886144b9a0
yukirang <rangk@163.com>
2019-07-22 23:29:35 -0700

    make it better

--- server.js
+++ server.js
@@ -4,30 +4,28 @@ const path = require('path');
 
 const app = express();
 
-//connect database
+// Connect Database
 connectDB();
 
-//init middleware
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-//
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-//serve static
-app.use(express.static(path.join(__dirname, 'client/build')));
-//production mode
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Set static folder
+  app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
-    res.sendfile(path.join((__dirname = 'client/build/index.html')));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-//define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
