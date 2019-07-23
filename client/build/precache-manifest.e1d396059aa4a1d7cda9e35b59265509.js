4ef709c88b132937775852bec9b26bdfd347c488
yukirang <rangk@163.com>
2019-07-22 22:09:02 -0700

    point to index

---
+++ client/build/precache-manifest.e1d396059aa4a1d7cda9e35b59265509.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06248f48e5825dfefceadaa00832a85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b22b0602c134fc3491e",
+    "url": "/static/css/main.7bb13baa.chunk.css"
+  },
+  {
+    "revision": "a5260eff201b59abb11b",
+    "url": "/static/js/2.5ceb2e70.chunk.js"
+  },
+  {
+    "revision": "9b22b0602c134fc3491e",
+    "url": "/static/js/main.2aebcc84.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
