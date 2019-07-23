e4920f7ff76af76701ccd5510c53f917f737ed78
yukirang <rangk@163.com>
2019-07-23 01:20:27 -0700

    EditProfile title

---
+++ client/build/precache-manifest.8a835db72813b5ac512de644827dd632.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6e5e1b12783d741e10993cb5a3c565",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbdc32bf0447749bd5e8",
+    "url": "/static/css/main.6117ae78.chunk.css"
+  },
+  {
+    "revision": "a5260eff201b59abb11b",
+    "url": "/static/js/2.5ceb2e70.chunk.js"
+  },
+  {
+    "revision": "cbdc32bf0447749bd5e8",
+    "url": "/static/js/main.5276ed9e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
