575153d56940df3a934f0eb2083c2cacd995114a
yukirang <rangk@163.com>
2019-07-22 17:38:34 -0700

    prepared for deployment

---
+++ client/build/precache-manifest.bf86c4b46782be08962cf738d55041e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaeacfd16241c9bec672853e1030ad7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "909b9eff34e6157fb4c5",
+    "url": "/static/css/main.7bb13baa.chunk.css"
+  },
+  {
+    "revision": "a5260eff201b59abb11b",
+    "url": "/static/js/2.5ceb2e70.chunk.js"
+  },
+  {
+    "revision": "909b9eff34e6157fb4c5",
+    "url": "/static/js/main.d8020c08.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
